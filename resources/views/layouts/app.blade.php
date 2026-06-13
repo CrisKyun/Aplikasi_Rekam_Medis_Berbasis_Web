d0c6289909dbb2582900d8c1676f2bf81a3073c5
Razaka <razakareza31@gmail.com>
2026-06-13 08:36:27 +0700

    tampilan menggunkan js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Praktik Mandiri dr. Luria Widijana Haribawanti.')</title>
+    {{-- Bootstrap --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    {{-- Bootstrap Icons --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+    {{-- CSS & JS Laravel / Vite --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-
 <body class="bg-light">
 
     {{-- Navbar --}}
